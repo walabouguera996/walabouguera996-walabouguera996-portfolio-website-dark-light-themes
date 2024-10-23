b3e69aa74781fa28c8dae2fc70b08597368587ff
walabouguera996 <walabouguera996@gmail.com>
2024-10-23 17:26:45 +0100

    project

---
+++ assets/js/email.js
@@ -0,0 +1,22 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_u4lx6i3";
+    const templateID = "template_vwx415o";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your response sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
---
+++ copyfiles.sh
@@ -0,0 +1,12 @@
+pwd
+# cd /Users/walabouguera/Documents/Code/portfolio-website-dark-light-themes
+cd  /Users/walabouguera/Documents/Code/portfolio-website-dark-light-themes/
+mkdir public
+cp /Users/walabouguera/Documents/Code/portfolio-website-dark-light-themes/index.html /Users/walabouguera/Documents/Code/portfolio-website-dark-light-themes/public/
+cp /Users/walabouguera/Documents/Code/portfolio-website-dark-light-themes/manifest.json /Users/walabouguera/Documents/Code/portfolio-website-dark-light-themes/public/
+cp /Users/walabouguera/Documents/Code/portfolio-website-dark-light-themes/serviceWorker.js /Users/walabouguera/Documents/Code/portfolio-website-dark-light-themes/public/
+cp /Users/walabouguera/Documents/Code/portfolio-website-dark-light-themes/404.html /Users/walabouguera/Documents/Code/portfolio-website-dark-light-themes/public/
+cd  /Users/walabouguera/Documents/Code/portfolio-website-dark-light-themes/public/
+mkdir assets
+cp -r /Users/walabouguera/Documents/Code/portfolio-website-dark-light-themes/assets/* /Users/walabouguera/Documents/Code/portfolio-website-dark-light-themes/public/assets
+# cp ../index.html ../public/
