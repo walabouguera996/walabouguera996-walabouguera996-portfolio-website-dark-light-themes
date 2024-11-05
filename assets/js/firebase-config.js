c9d8b862c08e9ea1133ec7802e7de2568831506d
walabouguera996 <walabouguera996@gmail.com>
2024-11-05 12:47:16 +0100

    firebase update

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,10 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyA8rWKMrMTZdMQcdE50oZoPeOiMIyAurNM",
+    authDomain: "dark-light-portfoli.firebaseapp.com",
+    databaseURL: "<your-database-url>",
+    projectId: "dark-light-portfoli",
+    storageBucket: "dark-light-portfoli.appspot.com",
+    messagingSenderId: "84803752381",
+    appId: "1:84803752381:web:196b4bf12a0ca309f702d4",
+    measurementId: "G-1Z2TCP561S"
+};
